762b59e5f8a36046114f57d0b20f1895ae1dff32
weiguoyuan <weiguoyuan@163.com>
2018-02-02 10:40:02 +0800

    add files

---
+++ src/main/java/weiguoyuanNetty/App.java
@@ -0,0 +1,13 @@
+package weiguoyuanNetty;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
